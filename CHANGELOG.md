ec302854228b42cc5c0361ae9c9c0f2a065b761d
Michael A. Bos <mab05k@gmail.com>
2020-05-25 13:58:17 -0700

    update stream client endpoints to return a stream for processing

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 # Change Log
 
+#### [ v0.4.0 ] Feature
+- Add better support for Stream Endpoints ([docs](src/Bridge/Symfony/Bundle/Resources/doc/usage/stream_endpoints.md))
+
 #### [ v0.3.4 ] Patch
 - Traits use FQDN in dockblocks ([reference](https://github.com/api-platform/core/issues/3349))
 
---
+++ tests/Unit/Helper/StreamResponseHelperTest.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of the Mab05k Oanda Client Bundle.
+ * (c) Michael A. Bos <mab05k@gmail.com>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Mab05k\OandaClient\Tests\Unit\Helper;
+
+use Mab05k\OandaClient\Helper\StreamResponseHelper;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\StreamInterface;
+
+class StreamResponseHelperTest extends TestCase
+{
+    /**
+     * @Mock
+     *
+     * @var StreamInterface
+     */
+    private $stream;
+
+    protected function setUp()
+    {
+        \Phake::initAnnotations($this);
+    }
+
+    public function testReadLine()
+    {
+        \Phake::when($this->stream)->eof()
+            ->thenReturn(false)
+            ->thenReturn(false)
+            ->thenReturn(false)
+            ->thenReturn(true);
+        \Phake::when($this->stream)->read(1)
+            ->thenReturn('a')
+            ->thenReturn('b')
+            ->thenReturn(PHP_EOL);
+
+        $result = StreamResponseHelper::readLine($this->stream);
+
+        $this->assertEquals('ab'.PHP_EOL, $result);
+    }
+}
